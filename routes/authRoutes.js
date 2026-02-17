5deabf2caf921c43a927ed67ca3aff7f83bfd375
integrate360 <integrate360@gmail.com>
2026-02-17 11:21:53 +0530

    auth

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,4 +1,4 @@
-import express from 'express';
+import express from "express";
 import {
   registerUser,
   loginUser,
@@ -9,21 +9,40 @@ import {
   getUserById,
   updateUserByAdmin,
   deactivateUser,
-} from '../controllers/authController.js';
-
-import authMiddleware from '../middleware/authmiddleware.js';
-import roleMiddleware from '../middleware/roleMiddleware.js';
+} from "../controllers/authController.js";
+import authMiddleware from "../middleware/authmiddleware.js";
+import roleMiddleware from "../middleware/roleMiddleware.js";
 
 const router = express.Router();
 
-router.post('/register', registerUser);
-router.post('/login', loginUser);
-router.get('/get/me', authMiddleware, getMyProfile);
-router.put('/update/me', authMiddleware, updateMyProfile);
-router.put('/change-password', authMiddleware, changePassword);
-router.get('/getallusers', authMiddleware, roleMiddleware('super_admin'), getAllUsers);
-router.get('/get-by/:id', authMiddleware, roleMiddleware('super_admin'), getUserById);
-router.put('/update-user/:id', authMiddleware, roleMiddleware('super_admin'), updateUserByAdmin);
-router.delete('/delete-user/:id', authMiddleware, roleMiddleware('super_admin'), deactivateUser);
+router.post("/register", registerUser);
+router.post("/login", loginUser);
+router.get("/get/me", authMiddleware, getMyProfile);
+router.put("/update/me", authMiddleware, updateMyProfile);
+router.put("/change-password", authMiddleware, changePassword);
+router.get(
+  "/getallusers",
+  authMiddleware,
+  roleMiddleware("super_admin"),
+  getAllUsers,
+);
+router.get(
+  "/get-by/:id",
+  authMiddleware,
+  roleMiddleware("super_admin"),
+  getUserById,
+);
+router.put(
+  "/update-user/:id",
+  authMiddleware,
+  roleMiddleware("super_admin"),
+  updateUserByAdmin,
+);
+router.delete(
+  "/delete-user/:id",
+  authMiddleware,
+  roleMiddleware("super_admin"),
+  deactivateUser,
+);
 
 export default router;
