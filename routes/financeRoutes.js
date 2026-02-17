cdd2c58353989e0e5ad9536da1729d39a831d98f
integrate360 <integrate360@gmail.com>
2026-02-17 11:12:21 +0530

    changes

--- routes/financeRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router  = express.Router();
-const ctrl    = require('../controllers/financeController');
-const { protect, authorize } = require('../middleware/auth');
-
-router.get('/dashboard', protect, authorize('super_admin', 'accountant'), ctrl.getDashboard);
-router.get('/execution/:projectId', protect, ctrl.getExecutionMonitoring);
-
-module.exports = router;
